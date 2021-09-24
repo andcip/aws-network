c2314f0c09bccbdb08c8da25730fcc1600205700
Andrea Cipriani <andrea.cipriani@assist.it>
2021-09-24 11:42:48 +0200

    Add subnets cidr as (nullable) input varibales

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 data "aws_availability_zones" "az" {}
 
 
@@ -6,78 +5,88 @@ data "aws_ec2_transit_gateway" "transit" {
   count = length(var.tg_routes) > 0 ? 1 : 0
 }
 
+locals {
+  total_subnets_count = var.subnets.private.count + var.subnets.public.count
+  subnets             = [
+      for i in range(local.total_subnets_count) :
+        cidrsubnet(var.cidr_block, ceil(log(local.total_subnets_count, 2 )), i )
+  ]
+  private_subnets = [ for i in range(var.subnets.private.count) : local.subnets[i]]
+  public_subnets = [for i in range(var.subnets.private.count, local.total_subnets_count) : local.subnets[i]]
+
+}
 
 ## VPC
 resource "aws_vpc" "main" {
-  cidr_block            = var.cidr_block
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
-  tags                  = merge(var.common_tags, {"Name": var.project_name})
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = merge(var.common_tags, { "Name" : var.project_name })
 }
 
 
 ## Public Subnets
 resource "aws_subnet" "public" {
-  count                   = var.public_subnets_count
+  count                   = var.subnets.public.count
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.az.names[count.index]
-  cidr_block              = cidrsubnet(var.cidr_block, ceil(log(var.public_subnets_count * 2, 2)), var.public_subnets_count + count.index )
+  cidr_block              = var.subnets.public.cidr != [] ? var.subnets.public.cidr[count.index] : local.public_subnets[count.index]
 
   tags = merge(var.common_tags,
   {
-    "Name"              = "public-subnet-${count.index}"
-    "AZ"                = data.aws_availability_zones.az.names[count.index]
-    "Type"              = "Public"
+    "Name" = "public-subnet-${count.index}"
+    "AZ"   = data.aws_availability_zones.az.names[count.index]
+    "Type" = "Public"
   })
 }
 
 ## Private Subnets
 resource "aws_subnet" "private" {
-  count                   = var.private_subnets_count
+  count                   = var.subnets.private.count
   vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = false
   availability_zone       = data.aws_availability_zones.az.names[count.index]
-  cidr_block              = cidrsubnet(var.cidr_block, ceil(log(var.private_subnets_count * 2, 2)), var.private_subnets_count + count.index )
+  cidr_block              = var.subnets.private.cidr != [] ? var.subnets.private.cidr[count.index] : local.private_subnets[count.index]
 
   tags = merge(var.common_tags,
   {
-    "Name"              = "private-subnet-${count.index}"
-    "AZ"                = data.aws_availability_zones.az.names[count.index]
-    "Type"              = "Private"
+    "Name" = "private-subnet-${count.index}"
+    "AZ"   = data.aws_availability_zones.az.names[count.index]
+    "Type" = "Private"
   })
 }
 
 ## Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  count     = length(var.public_subnets_count) > 0 ? 1 : 0
-  vpc_id    = aws_vpc.main.id
-  tags      = var.common_tags
+  count  = var.subnets.public.count > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
+  tags   = var.common_tags
 }
 
 ## Elastic IP for the NAT Gateway
 resource "aws_eip" "nat" {
-  count = var.public_subnets_count
+  count = var.subnets.public.count
   vpc   = true
-  tags  = merge(var.common_tags,{Name = "natgw-eip-${count.index}"})
+  tags  = merge(var.common_tags, { Name = "natgw-eip-${count.index}" })
 }
 
 ## NAT Gateway
 resource "aws_nat_gateway" "ng" {
-  count = var.public_subnets_count
+  count         = var.subnets.public.count
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
-  depends_on = [aws_internet_gateway.igw[0]]
-  tags = merge( var.common_tags, { Name = "natgw-${count.index}"})
+  depends_on    = [aws_internet_gateway.igw[0]]
+  tags          = merge( var.common_tags, { Name = "natgw-${count.index}" })
 }
 
 ## Private Route Table
 resource "aws_route_table" "private" {
-  count = var.private_subnets_count
+  count  = var.subnets.private.count
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ng[count.index % length(aws_nat_gateway.ng)].id
   }
 
@@ -85,7 +94,7 @@ resource "aws_route_table" "private" {
   dynamic "route" {
     for_each = var.tg_routes
     content {
-      cidr_block = route.value
+      cidr_block         = route.value
       transit_gateway_id = data.aws_ec2_transit_gateway.transit[0].id
     }
   }
@@ -95,24 +104,25 @@ resource "aws_route_table" "private" {
 
 ## Public Route Table
 resource "aws_route_table" "public" {
-  vpc_id        = aws_vpc.main.id
+  count  = var.subnets.public.count > 0 ? 1 : 0
+  vpc_id = aws_vpc.main.id
   route {
-    cidr_block  = "0.0.0.0/0"
-    gateway_id  = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw[count.index].id
   }
-  tags          = merge(var.common_tags, { Name = "public-rt" })
+  tags   = merge(var.common_tags, { Name = "public-rt" })
 }
 
 
 resource "aws_route_table_association" "public" {
-  count          = var.public_subnets_count
+  count          = var.subnets.public.count
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public[count.index].id
 }
 
 resource "aws_route_table_association" "private" {
-  count = var.private_subnets_count
-  subnet_id = aws_subnet.private[count.index].id
+  count          = var.subnets.private.count
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
 
@@ -125,7 +135,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tg_vpc_attachment" {
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
-  count       = length(aws_subnet.private)
+  count       = var.subnets.private.count
   name        = "/infrastructure/vpc/subnets/private/${count.index}/id"
   description = "Export the private subnet ${count.index} id"
   type        = "String"
--- variables.tf
+++ variables.tf
@@ -7,16 +7,32 @@ variable "tg_routes" {
   default = []
 }
 
-variable "public_subnets_count" {
-  type = number
-  default = 2
-}
-
-variable "private_subnets_count" {
-  type = number
-  default = 2
-}
-
 variable "common_tags" {
   type = map(string)
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "subnets" {
+  type = object({
+    private: object({
+      count: number,
+      cidr: list(string)
+    }),
+    public: object({
+      count: number,
+      cidr: list(string)
+    })
+  })
+  default = {private: {count:2, cidr: []}, public: {count:2, cidr: []}}
+  validation {
+    condition = var.subnets.private.cidr == [] || length(var.subnets.private.cidr) == var.subnets.private.count
+    error_message = "Private Subnets CIDR size must be equal to private subnets count."
+  }
+  validation {
+    condition = var.subnets.public.cidr == [] || length(var.subnets.public.cidr) == var.subnets.public.count
+    error_message = "Public Subnets CIDR size must be equal to public subnets count."
+  }
 }

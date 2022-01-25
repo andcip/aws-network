cef690624156e8714ba6d4ed46eade1003078dae
Andrea Cipriani <andrea.cipriani@assist.it>
2022-01-25 19:00:31 +0100

    Add variable description

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "cidr_block" {
-  type = string
+  type        = string
+  description = "The CIDR block to use fot the VPC."
 }
 
 variable "tg_routes" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
+  description = "List of subnets to route to the transit gateway."
 }
 
 variable "project_name" {
-  type = string
+  type        = string
+  description = "The project name, must not be empty"
 }
 
 variable "vpc_flow_log_enabled" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Enable the VPC flow logs, default disabled"
 }
 
 variable "subnets" {
@@ -30,6 +34,8 @@ variable "subnets" {
 
   default = { private : { count : 2, cidr : [] }, public : { count : 2, cidr : [] } }
 
+  description = "Utility object to specify private and public subnets. You can choose the number and the cidr of everyone. Default goes to 2 private and 2 public and the relative cidrs are calculated based on vpc"
+
   validation {
     condition     = length(var.subnets.private.cidr) == 0 || length(var.subnets.private.cidr) == var.subnets.private.count
     error_message = "Private Subnets CIDR size must be equal to private subnets count."
@@ -41,8 +47,10 @@ variable "subnets" {
 }
 
 variable "vpc_endpoints" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
+  description = "List of vpc endpoint to enable."
+
   validation {
     condition     = alltrue([
     for vpce in var.vpc_endpoints : contains(["s3", "sns", "execute-api", "dynamodb", "ecr.dkr", "ecr.api"], vpce)
@@ -52,12 +60,13 @@ variable "vpc_endpoints" {
 }
 
 variable "bastion" {
-  type = object({
+  type        = object({
     enabled : bool,
     certificate_name : string,
     certificate_key : string
   })
-  default = {enabled: false, certificate_name: null, certificate_key: null}
+  default     = { enabled : false, certificate_name : null, certificate_key : null }
+  description = "Choose if enable bastion host, with the given ssh certificate"
 
   validation {
     condition = var.bastion.enabled ? var.bastion.certificate_name != null && var.bastion.certificate_key != null : true

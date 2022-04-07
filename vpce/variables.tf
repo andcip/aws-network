836835566f9e7c280581efcad8376d364bac5244
andcas <andrea.casadei@assistdigital.com>
2022-04-07 18:29:20 +0200

    add states vpce instance

--- vpce/variables.tf
+++ vpce/variables.tf
@@ -1,11 +1,6 @@
 variable "vpc_endpoints" {
   type    = list(string)
   default = []
-  validation {
-    condition     = contains(var.vpc_endpoints, "s3") || contains(var.vpc_endpoints, "sns") || contains(var.vpc_endpoints, "execute-api") || contains(var.vpc_endpoints, "dynamodb") || contains(var.vpc_endpoints, "rds")
-
-    error_message = "Invalid VPC Endpoint service, allowed values are s3, sns, execute-api, dynamodb, rds."
-  }
 }
 
 variable "vpc" {
